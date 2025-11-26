81076929af2647993220d83a0f49825fc36a5bbc
senaC <62671894+alt218@users.noreply.github.com>
2025-11-26 09:29:43 +0900

    Create R_PG_IO_PORT.c

---
+++ PDG/IO/R_PG_IO_PORT.c
@@ -0,0 +1,78 @@
+/******************************************************************************
+* DISCLAIMER
+
+* This software is supplied by Renesas Electronics Corporation and is only 
+* intended for use with Renesas products. No other uses are authorized.
+
+* This software is owned by Renesas Electronics Corporation and is protected under 
+* all applicable laws, including copyright laws.
+
+* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES
+* REGARDING THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, 
+* INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+* PARTICULAR PURPOSE AND NON-INFRINGEMENT.  ALL SUCH WARRANTIES ARE EXPRESSLY 
+* DISCLAIMED.
+
+* TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS 
+* ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE 
+* FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES 
+* FOR ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR ITS 
+* AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+
+* Renesas reserves the right, without notice, to make changes to this 
+* software and to discontinue the availability of this software.  
+* By using this software, you agree to the additional terms and 
+* conditions found by accessing the following link:
+* http://www.renesas.com/disclaimer
+******************************************************************************
+* Copyright (C) 2010-2013 Renesas Electronics Corporation.
+* and Renesas Solutions Corporation. All rights reserved.
+******************************************************************************
+* File Name    : R_PG_IO_PORT.c
+* Version      : 1.00
+* Device(s)    : 
+* Tool-Chain   : 
+* H/W Platform : 
+* Description  : 
+* Limitations  : 
+******************************************************************************
+* History : 19.11.2025 Version Description
+*         :   
+******************************************************************************/
+
+
+/******************************************************************************
+Includes   <System Includes> , "Project Includes"
+******************************************************************************/
+#include "r_pdl_io_port.h"
+#include "r_pdl_definitions.h"
+
+
+/******************************************************************************
+* ID           : 
+*
+* Include      : 
+*
+* Declaration  : bool R_PG_IO_PORT_SetPortNotAvailable(void)
+*
+* Function Name: R_PG_IO_PORT_SetPortNotAvailable
+*
+* Description  : ���݂��Ȃ��[�q�̏���
+*
+* Arguments    : �Ȃ�
+*
+* Return Value : true  : �ݒ肪�������s��ꂽ�ꍇ
+*              : false : �ݒ�Ɏ��s�����ꍇ
+*
+* Calling Functions : R_IO_PORT_NotAvailable
+*
+* Details      : �ڍׂɂ��Ă̓��t�@�����X�}�j���A�����Q�Ƃ��Ă��������B
+******************************************************************************/
+bool R_PG_IO_PORT_SetPortNotAvailable(void)
+{
+	return R_IO_PORT_NotAvailable();
+
+}
+
+
+
